b3208f1fa1874bba4a37bf28bade1073efaf42fa
paulmrg-461 <paulmrg461@gmail.com>
2025-12-30 09:22:31 -0500

    Seccion5: Abstract Factory pattern 6.1 pattern

--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -12,8 +12,67 @@
  * https://refactoring.guru/es/design-patterns/abstract-factory
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  *  El propósito del Abstract Factory es crear familias de objetos relacionados
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+
+interface Hamburger {
+    prepare(): void;
+}
+
+interface Drink {
+    serve(): void;
+}
+
+class VeggieBurger implements Hamburger {
+    prepare(): void {
+      console.log(`Preparing a %cVeggie Burger.`, COLORS.green);
+    }
+}
+
+class BeefBurger implements Hamburger {
+    prepare(): void {
+      console.log(`Preparing a %cBeef Burger.`, COLORS.red);
+    }
+}
+
+class Water implements Drink {
+  serve(): void {
+    console.log(`Serving %cWater`, COLORS.blue);
+  }
+}
+
+class Beer implements Drink {
+  serve(): void {
+    console.log(`Serving %cBeer`, COLORS.yellow);
+  }
+}
+
+interface RestaurantFactory {
+  createHamburger(): Hamburger;
+  serveDrink(): Drink;
+}
+
+class VeggieRestaurantFactory implements RestaurantFactory {
+  createHamburger(): Hamburger {
+    return new VeggieBurger();
+  }
+
+  serveDrink(): Drink {
+    return new Water();
+  }
+}
+
+class FastFoodRestaurantFactory implements RestaurantFactory {
+  createHamburger(): Hamburger {
+    return new BeefBurger();
+  }
+
+  serveDrink(): Drink {
+    return new Beer();
+  }
+}
